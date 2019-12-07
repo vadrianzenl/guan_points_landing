f52620251039c1d09c6373cf7abe66c3ebe63afb
Victor <vadrianzenl@gmail.com>
2019-12-07 11:58:36 -0500

    [feat] Change ADS text

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,7 +9,7 @@ const Header = () => {
             <div className="col-lg-6">
               <div className="text-container">
                 <h1><span className="turquoise">Guan</span> <br />Gana en Guan</h1>
-                <p className="p-large">Guan, es una plataforma de lealtad y marketing digital que conecta tu negocio con tus clientes de una manera divertida</p>
+                <p className="p-large">ADS, es una plataforma de lealtad y marketing digital que conecta tu negocio con tus clientes de una manera divertida</p>
                 <a className="btn-solid-lg page-scroll" href="#services">DESCUBRIR</a>
               </div>
             </div>
